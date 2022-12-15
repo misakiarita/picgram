6c40ac870c15310757a65a367ac5d64beb5574bc
misaki arita <misaki.arita0838@gmail.com>
2022-12-15 21:58:41 +0900

    新規投稿ページの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,67 @@
 class PostsController < ApplicationController
+before_action :set_feed, only: %i[ show edit update destroy]
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def confirm
+    @post = current_user.posts.build(post_params)
+    render :new if @post.invalid?
+    end
+
+  def create
+    @post = Post.new(post_params)
+    if params[:back]
+        render :new
+    else 
+        if @post.save
+            redirect_to new_post_path
+        else
+            render :new
+        end
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    redirect_to posts_path
+    end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+        redirect_to post_path, notice: "Your post edited!"
+    else
+        render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id]) 
+    @post.destroy
+    redirect_to posts_path, notice:"Your post deleted!"
+  end
+
+  def confirm
+    @post = Post.new(post_params)
+    render :new if @post.invalid?
+  end
+
+  private
+
+  def set_feed
+    @post = Post.find(params[:id])
+    end
+
+  def post_params
+    params.require(:post).permit(:image, :image_cache, :content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :posts do
+    collection do
+      post :confirm
+      patch :confirm
+    end
+  end
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
 end
