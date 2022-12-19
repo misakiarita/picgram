ba55b8403177b0a02931d3af48b2c493101fffe0
misaki arita <misaki.arita0838@gmail.com>
2022-12-19 20:50:12 +0900

    本番環境でLetterOpnerを実装、エラー修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,6 +117,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.default_url_options = { host: 'mysterious-eyrie-01618.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://mysterious-eyrie-01618.herokuapp.com' }
   config.action_mailer.delivery_method = :letter_opener_web
+  ActionMailer::Base.delivery_method = :smtp
 end
